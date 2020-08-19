f42543813edf1061c26e9114be4390a6d397dfbf
Ivo F. Carvalho <ivo.f.carvalho@noesis.pt>
2020-08-19 18:32:18 +0100

    Added code for form; Added php to docker-compose.yml;

---
+++ webpage/frontend/index.php
@@ -0,0 +1,64 @@
+<?php
+
+if (isset($_POST['form-submit'])) {
+    // warn user about the errors and kill function
+    function problem($error)
+    {
+        echo "<script type='text/javascript'>alert('$error');</script>";
+        die();
+    }
+
+    // check if there is injection in form
+    function IsInjected($str){
+        $injections = array('(\n+)',
+            '(\r+)',
+            '(\t+)',
+            '(%0A+)',
+            '(%0D+)',
+            '(%08+)',
+            '(%09+)'
+        );
+
+        $inject = join('|', $injections);
+        $inject = "/$inject/i";
+
+        if(preg_match($inject,$str))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // get to email from the page
+//    if (!isset($_POST['to-email'])){
+//        problem('There is a problem with the page email.');
+//    }
+    $to_email = "ivofdcarvalho@gmail.com";
+
+    // validation expected data exists
+    if (!isset($_POST['form-name']) || !isset($_POST['form-email']) || !isset($_POST['form-subject']) || !isset($_POST['form-message'])){
+        problem('We are sorry, but there appears to be a problem with the form you submitted.');
+    }
+
+    $name = $_POST['form-name'];
+    $from_email = $_POST['form-email'];
+    $subject = $_POST['form-subject'];
+    $message = $_POST['form-message'];
+
+    // check for injection
+    if(IsInjected($to_email) || IsInjected($name) || IsInjected($from_email) || IsInjected($subject) || IsInjected($message)){
+        problem('We are sorry, but there form cannot be submitted with the data provided.');
+    }
+
+    $message_email = "Form details below.\r\n";
+    $message_email .= "Name: $name \r\n";
+    $message_email .= "Email: $from_email \r\n";
+    $message_email .= "Message: $message \r\n";
+
+    @mail($to_email, $subject, $message_email);
+
+    echo "<script type='text/javascript'>alert('Thank you for the contact. We will be in touch with you very soon.');window.location.href='index.html'</script>";
+}
